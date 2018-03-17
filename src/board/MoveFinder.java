2c1d23147121e147d61871ab51a8a092d5ec1d5f
danstalcup <billyjoelntrdame@gmail.com>
2018-03-17 12:42:01 -0400

    Add moveFinder and build out PieceFactory

---
+++ src/board/MoveFinder.java
@@ -0,0 +1,20 @@
+package board;
+
+import pieces.PieceFactory;
+
+import java.util.Vector;
+
+public class MoveFinder {
+    public String[] FindMoves(Board board)
+    {
+        Vector<String> moves = new Vector<String>();
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                moves.addAll(PieceFactory.BuildPiece(board.getPiece(i,j)).GetLegalMoves(i,j,board));
+            }
+        }
+        return moves.toArray(new String[0]);
+    }
+}
--- src/pieces/PieceFactory.java
+++ src/pieces/PieceFactory.java
@@ -5,7 +5,13 @@ public class PieceFactory {
     {
         Piece result = null;
 
-        if(PieceHelper.Is(pieceByte,Piece.PAWN)) result = new Pawn(pieceByte);
+        if(PieceHelper.Is(pieceByte,Piece.EMPTY)) result = new Empty(pieceByte);
+        else if(PieceHelper.Is(pieceByte,Piece.PAWN)) result = new Pawn(pieceByte);
+        else if(PieceHelper.Is(pieceByte,Piece.KNIGHT)) result = new Knight(pieceByte);
+        else if(PieceHelper.Is(pieceByte,Piece.BISHOP)) result = new Bishop(pieceByte);
+        else if(PieceHelper.Is(pieceByte,Piece.ROOK)) result = new Rook(pieceByte);
+        else if(PieceHelper.Is(pieceByte,Piece.QUEEN)) result = new Queen(pieceByte);
+        else if(PieceHelper.Is(pieceByte,Piece.KING)) result = new King(pieceByte);
 
         return result;
     }
