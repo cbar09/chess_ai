f4bbc517be5c90ea6c4d2b9f97ec53af5d6bb070
danstalcup <billyjoelntrdame@gmail.com>
2018-04-29 15:52:06 -0400

    Add normal move search for Rook

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -11,7 +11,14 @@ public class Rook extends Piece {
 
     @Override
     public Vector<String> GetLegalNormalMoves(int rank, int file, Board board) {
-        return new Vector<String>();
+        Vector<String> result = new Vector<String>();
+
+        result.addAll(GetLegalMovesInPath(rank,file,board,1,0));
+        result.addAll(GetLegalMovesInPath(rank,file,board,-1,0));
+        result.addAll(GetLegalMovesInPath(rank,file,board,0,1));
+        result.addAll(GetLegalMovesInPath(rank,file,board,0,-1));
+
+        return result;
     }
 
     @Override
