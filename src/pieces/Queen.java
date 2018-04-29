353f8644de6812f4c3f33eacf7c3dd64255e874f
danstalcup <billyjoelntrdame@gmail.com>
2018-04-29 15:51:56 -0400

    Add normal move search for Queen

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -11,7 +11,19 @@ public class Queen extends Piece {
 
     @Override
     public Vector<String> GetLegalNormalMoves(int rank, int file, Board board) {
-        return new Vector<String>();
+        Vector<String> result = new Vector<String>();
+
+        result.addAll(GetLegalMovesInPath(rank,file,board,0,1));
+        result.addAll(GetLegalMovesInPath(rank,file,board,0,-1));
+        result.addAll(GetLegalMovesInPath(rank,file,board,1,0));
+        result.addAll(GetLegalMovesInPath(rank,file,board,-1,0));
+
+        result.addAll(GetLegalMovesInPath(rank,file,board,1,1));
+        result.addAll(GetLegalMovesInPath(rank,file,board,1,-1));
+        result.addAll(GetLegalMovesInPath(rank,file,board,-1,1));
+        result.addAll(GetLegalMovesInPath(rank,file,board,-1,-1));
+
+        return result;
     }
 
     @Override
